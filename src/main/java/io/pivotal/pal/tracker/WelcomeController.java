a78d742144e2ace39d83bd06973d1c60624e6ac9
Jena Jordahl <jjordahl.ctr@kr.af.mil>
2020-06-25 07:12:51 +0000

    Finished Configuring an App

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String myhello;
+
+    public WelcomeController(@Value("${welcome.message}") String welcome) {
+        myhello = welcome;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return myhello;
     }
 }
